150075524a7c2ccdf050369b12d7c5a62fc9d50b
Yash Raj Singh <116808459+JuxtaRYCT@users.noreply.github.com>
2023-04-12 20:41:06 +0530

    Add files via upload

--- Technical/SymbolicEquationSolver.py
+++ Technical/SymbolicEquationSolver.py
@@ -5,19 +5,39 @@ Created on Wed Apr 12 01:17:32 2023
 
 @author: JuxtaR_YCT
 """
-explist=[]
-print("Enter the equation: ")
+import re
+import math
+
+
+
+# explist=[]
+# m=[]
+print("Enter the quadratic equation in the form of ax^2+bx+c=0 : ")
 eqn=input()
-a=0
-for i in eqn:
-    explist.append(i)
-print(explist)
-for i in range(len(explist)):
-    if(explist[i]=="^" and explist[i+1]=="2"):
-        if(explist[0].isalpha()==False and explist[0]=="-"):
-            a=explist[1]
-            a=int(a)
-            a=-a
-        else:
-            a=explist[0]
-            a=int(a)
+
+checker=r'^([+-]?\d+)x\^2([+-]\d+x)?([+-]\d+)?$'
+check=re.match(checker,eqn)
+
+# a=0
+# l=eqn.split("+")
+# for i in range(len(l)):
+#     m.append(l[i].split("-"))
+# print(m)
+
+# for i in eqn:
+#     explist.append(i)
+# print(explist)
+# for i in range(len(explist)):
+#     if(explist[i]=="^" and explist[i+1]=="2"):
+#         if(explist[0].isalpha()==False and explist[0]=="-"):
+#             a=explist[1]
+#             a=int(a)
+#             a=-a
+            
+#         else:
+#             a=explist[0]
+#             a=int(a)
+
+            
+            
+
