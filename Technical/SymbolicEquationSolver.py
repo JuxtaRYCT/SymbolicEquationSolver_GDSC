387cbba66706d4c23ab9e7311b143c191c15dc09
Yash Raj Singh <116808459+JuxtaRYCT@users.noreply.github.com>
2023-04-12 20:55:22 +0530

    Add files via upload

--- Technical/SymbolicEquationSolver.py
+++ Technical/SymbolicEquationSolver.py
@@ -12,11 +12,38 @@ import math
 
 # explist=[]
 # m=[]
-print("Enter the quadratic equation in the form of ax^2+bx+c=0 : ")
+print("Enter the equation in the form of ax^2+bx+c=0 : ")
 eqn=input()
+
 
 checker=r'^([+-]?\d+)x\^2([+-]\d+x)?([+-]\d+)?$'
 check=re.match(checker,eqn)
+
+if check:
+    a = int(check.group(1))
+    b = int(check.group(2).replace("x", "")) if check.group(2) else 0
+    c = int(check.group(3)) if check.group(3) else 0
+    d=0
+    d=float(d)
+    
+    
+    d=b**2-4*a*c
+    
+    if(d==0):
+        root1=0
+        root1=float(root1)
+        root1=-b/(2*a)
+    elif(d<0):
+        imaginary=0
+        imaginary=float(imaginary)
+        real=0
+        real=float(real)
+        imaginary=(abs(b^2-4*a*c)/(2*a))**0.5
+        real=-b/(2*a)
+        print("The solutions to the equation are:\n Root-1: " ,real,"+",imaginary,"i")
+        
+
+
 
 # a=0
 # l=eqn.split("+")
