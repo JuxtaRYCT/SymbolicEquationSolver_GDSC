f3b99526c579e88c7be6b9bbfb6bca2f3d1f61c4
Yash Raj Singh <116808459+JuxtaRYCT@users.noreply.github.com>
2023-04-12 16:07:43 +0530

    Add files via upload

--- Technical/SymbolicEquationSolver.py
+++ Technical/SymbolicEquationSolver.py
@@ -1,1 +1,23 @@
-def
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 12 01:17:32 2023
+
+@author: JuxtaR_YCT
+"""
+explist=[]
+print("Enter the equation: ")
+eqn=input()
+for i in eqn:
+    explist.append(i)
+print(explist)
+for i in explist:
+    if(i=="^"):
+        if(explist[i+1]==2):
+    
+            if(explist[0].isalpha()==False):
+                a=explist[0]
+                a=int(a)
+            else:
+                a=1
+for i i
