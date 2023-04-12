a6037d476860095265bff4c282c02602c287bf43
Yash Raj Singh <116808459+JuxtaRYCT@users.noreply.github.com>
2023-04-12 17:00:25 +0530

    Add files via upload

--- Technical/SymbolicEquationSolver.py
+++ Technical/SymbolicEquationSolver.py
@@ -8,16 +8,16 @@ Created on Wed Apr 12 01:17:32 2023
 explist=[]
 print("Enter the equation: ")
 eqn=input()
+a=0
 for i in eqn:
     explist.append(i)
 print(explist)
-for i in explist:
-    if(i=="^"):
-        if(explist[i+1]==2):
-    
-            if(explist[0].isalpha()==False):
-                a=explist[0]
-                a=int(a)
-            else:
-                a=1
-for i i
+for i in range(len(explist)):
+    if(explist[i]=="^" and explist[i+1]=="2"):
+        if(explist[0].isalpha()==False and explist[0]=="-"):
+            a=explist[1]
+            a=int(a)
+            a=-a
+        else:
+            a=explist[0]
+            a=int(a)
