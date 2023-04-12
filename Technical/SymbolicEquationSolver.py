6940599c587f882181df7d8fc945f8895287f191
Yash Raj Singh <116808459+JuxtaRYCT@users.noreply.github.com>
2023-04-12 21:10:07 +0530

    Add files via upload

--- Technical/SymbolicEquationSolver.py
+++ Technical/SymbolicEquationSolver.py
@@ -6,33 +6,27 @@ Created on Wed Apr 12 01:17:32 2023
 @author: JuxtaR_YCT
 """
 import re
-import math
-
-
-
-# explist=[]
-# m=[]
 print("Enter the equation in the form of ax^2+bx+c=0 : ")
 eqn=input()
 
 
-checker=r'^([+-]?\d+)x\^2([+-]\d+x)?([+-]\d+)?$'
-check=re.match(checker,eqn)
+matcher=r'^([+-]?\d+)x\^2([+-]\d+x)?([+-]\d+)?$'
+match=re.match(matcher,eqn)
 
-if check:
-    a = int(check.group(1))
-    b = int(check.group(2).replace("x", "")) if check.group(2) else 0
-    c = int(check.group(3)) if check.group(3) else 0
+if match:
+    a = float(match.group(1))
+    b = float(match.group(2).replace("x", "")) if match.group(2) else 0
+    c = float(match.group(3)) if match.group(3) else 0
     d=0
     d=float(d)
-    
-    
     d=b**2-4*a*c
-    
-    if(d==0):
+    if(a==0):
+        print("This is not a quadratic equation.")
+    elif(d==0):
         root1=0
         root1=float(root1)
         root1=-b/(2*a)
+        print("There exists only one root for this equation which is:\n",root1)
     elif(d<0):
         imaginary=0
         imaginary=float(imaginary)
@@ -40,11 +34,22 @@ if check:
         real=float(real)
         imaginary=(abs(b^2-4*a*c)/(2*a))**0.5
         real=-b/(2*a)
-        print("The solutions to the equation are:\n Root-1: " ,real,"+",imaginary,"i")
+        print("The solutions to the equation are:\n Root-1: " ,real,"+",imaginary,"i\n","Root-2: ",real,"-",imaginary,"i\n")
+    elif(d>0):
+        root1=0
+        root1=float(root1)
+        root2=0
+        root2=float(root2)
+        root1=(-b+(b**2-4*a*c))/(2*a)
+        root2=(-b-(b**2-4*a*c))/(2*a)
+        print("The solutions to the equation are:\n Root-1: ",root1,"\nRoot-2: ",root2)
+        
+        
         
 
 
-
+# explist=[]
+# m=[]
 # a=0
 # l=eqn.split("+")
 # for i in range(len(l)):
